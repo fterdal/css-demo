657b5d78e5183a5513cb59ae2b4c2d29c8bb36d0
Finn <finnterdal@gmail.com>
2019-11-06 19:31:07 -0600

    finished event delegation demo

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+const faq = document.getElementById('faq')
+// const navList = document.getElementsByClassName('nav-list')[0]
+const navList = document.getElementById('nav-list')
+
+// console.log('navList[0]', navList[0])
+
+console.log(navList)
+
+navList.addEventListener('click', function(event) {
+  if (event.target.tagName === 'LI') {
+    event.target.style.opacity = '20%'
+  }
+})
+
 // console.log(document)
 
-const navItems = ['Home', 'About', 'FAQ', 'Contact Us']
+// const navItems = ['Home', 'About', 'FAQ', 'Contact Us']
 
-const [home, about] = navItems
+// const [home, about] = navItems
 
-console.log('home', home)
-console.log('about', about)
+// console.log('home', home)
+// console.log('about', about)
 
-const query = document.getElementsByTagName('nav')
-const [navBar] = query
+// const query = document.getElementsByTagName('nav')
+// const [navBar] = query
 
-navItems.forEach(item => {
-  const newNode = document.createElement('li')
-  newNode.innerText = item
-  navBar.appendChild(newNode)
-})
+// navItems.forEach(item => {
+//   const newNode = document.createElement('li')
+//   newNode.innerText = item
+//   navBar.appendChild(newNode)
+// })
 // navBar
 
 // console.log(navbar)
